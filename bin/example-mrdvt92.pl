ad6753a8d306652faaa93595487673a849ba94bf
Michael R. Davis <mrdvt@cpan.org>
2007-03-15 01:39:55 +0000

    Import of MRDVT/GD-Graph-Polar-0.15 from CPAN.
    gitpan-cpan-distribution: GD-Graph-Polar
    gitpan-cpan-version:      0.15
    gitpan-cpan-path:         MRDVT/GD-Graph-Polar-0.15.tar.gz
    gitpan-cpan-author:       MRDVT
    gitpan-cpan-maturity:     released

--- bin/example-mrdvt92.pl
+++ bin/example-mrdvt92.pl
@@ -6,7 +6,7 @@ example-mrdvt92.pl - GD::Graph::Polar example
 
 =head1 SAMPLE OUTPUT
 
-L<http://search.cpan.org/src/MRDVT/GD-Graph-Polar-0.14/bin/example-plot.png>
+L<http://search.cpan.org/src/MRDVT/GD-Graph-Polar-0.14/bin/example-mrdvt92.png>
 
 =cut
 
